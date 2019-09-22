eb2ae9d63d01fc464b54dfe050df0ed3df4c5b5b
Simon Perriard <simon.perriard@epfl.ch>
2019-09-22 12:08:35 +0200

    Added normalize and attempt for lsq_SGD

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+def normalize(X):
+    mean = np.mean(X)
+    std = np.std(x)
+    
+    return (X-mean) / std
+
 def MSE(y, X, w):
     N = X.shape[0]
     
@@ -21,3 +27,26 @@ def least_squares_GD(y, tx, initial_w, max_iter, gamma):
             next_w = current_w - gamma * MSE_dw(y, tx, current_w)
             
         return (next_w, MSE(y, tx, next_w))
+    
+    
+def least_squares_SGD(y, tx, initial_w, max_iter, gamma):
+    
+    next_w = initial_w
+    
+    mini_batch_size = 1
+    n_params = tx.shape[1]
+    batch_shape = (mini_batch_size, n_params)
+
+    random_indexes = np.zeros((1, mini_batch_size))
+    
+    for i in range(max_iter):
+        
+        random_indexes = np.random.uniform(0, tx.shape[0], mini_batch_size)
+        
+        mini_batch_X = tx[random_indexes]
+        mini_batch_y = y[random_indexes]
+        
+        current_w = next_w
+        next_w = current_w - gamma * MSE_dw(mini_batch_y, mini_batch_X, current_w)
+    
+    return (next_w, MSE(y, tx, new_w))
